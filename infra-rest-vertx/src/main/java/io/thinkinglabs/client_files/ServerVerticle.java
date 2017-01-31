8a9b17468b6be4007130274b5146f4f57c209089
Thierry de Pauw <thierry@thinkinglabs.io>
2017-01-31 20:32:20 +0100

    use vertx-web for REST api

--- infra-rest-vertx/src/main/java/io/thinkinglabs/client_files/ServerVerticle.java
+++ infra-rest-vertx/src/main/java/io/thinkinglabs/client_files/ServerVerticle.java
@@ -2,6 +2,8 @@ package io.thinkinglabs.client_files;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 /**
  * @author thipau
@@ -12,11 +14,16 @@ public class ServerVerticle extends AbstractVerticle
     @Override
     public void start(final Future<Void> future) throws Exception
     {
-        vertx.createHttpServer()
-                .requestHandler(r -> {
-                    r.response().end("<h1>Hello from my first " +
+        Router router = Router.router(vertx);
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type", "text/html")
+                    .end("<h1>Hello from my first " +
                             "Vert.x 3 application</h1>");
-                })
+        });
+
+        vertx.createHttpServer()
+                .requestHandler(router::accept)
                 .listen(8080, result -> {
                     if (result.succeeded()) {
                         future.complete();
