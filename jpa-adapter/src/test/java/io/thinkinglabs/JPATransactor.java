46187a4ee916abaecab8d79a43f884efb8e03532
Thierry de Pauw <thierry@thinkinglabs.io>
2017-03-13 18:36:35 +0100

    remove commented out transaction demarcation for querying the database

--- jpa-adapter/src/test/java/io/thinkinglabs/JPATransactor.java
+++ jpa-adapter/src/test/java/io/thinkinglabs/JPATransactor.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceException;
 
+/**
+ * @author @tdpauw
+ */
 public class JPATransactor implements Transactor {
 	private final EntityManager entityManager;
 
@@ -29,16 +32,12 @@ public class JPATransactor implements Transactor {
 	@Override
 	public Object performQuery(QueryUnitOfWork queryUnitOfWork)
 			throws Exception {
-//		EntityTransaction transaction = entityManager.getTransaction();
 		Object result = null;
 		try {
-//			transaction.begin();
 			result = queryUnitOfWork.query();
-//			transaction.commit();
 		} catch (PersistenceException e) {
 			throw e;
 		} catch (Exception e) {
-//			transaction.rollback();
 			throw e;
 		}
 		return result;
